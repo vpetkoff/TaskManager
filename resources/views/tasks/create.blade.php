f0ffeb67484304ca7e9cc94d44eb399f7016aa22
Vasil Petkov <vpetkoff88@gmail.com>
2018-06-12 20:50:39 +0300

    Added bootstrap class col-md-12 to the div displaying the error in create.blade.php

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
 
     <!-- Monitor for validation errors -->
     @if (count($errors) > 0)
-      <div class = "alert alert-danger">
+      <div class = "col-md-12 alert alert-danger">
         <ul>
           @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
