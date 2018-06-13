3f6914708cd530031b17537cf8b958cb02aeb278
Vasil Petkov <vpetkoff88@gmail.com>
2018-06-13 17:44:38 +0300

    Improved frontend accessibility - added an 'Edit' button to the index page for each task.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,7 @@
         <p>Last update: {{ $task->updated_at }}</p>
         <div class="row">
           <a href="/tasks/{{ $task->id }}" class="btn btn-secondary" style="margin-right:2px;">Read more</a>
+          <a href="/tasks/edit/{{ $task->id }}" class="btn btn-secondary" style="margin-right:2px;">Edit</a>
           <form method="POST" action={{ url("tasks/delete/$task->id") }}>
             @csrf
             @method('DELETE')
